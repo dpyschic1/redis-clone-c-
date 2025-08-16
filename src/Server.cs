ff3748d22cb54e4d016bcfe50f8a91941a3ab277
Mubashir Soomro <mubashir.soomro@stayify.com>
2025-08-16 08:01:30 +0500

    Lesson 2 ping response

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,29 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+Byte[] bytes = [];
+String data = null;
+
+while (true)
+{
+    Console.WriteLine("Waiting for connection... ");
+
+    using var sock = server.AcceptSocket(); // wait for client
+    Console.WriteLine("Socket accepted");
+    sock.Receive(bytes);
+    var recieved = Encoding.UTF8.GetString(bytes);
+    Console.WriteLine("Recieved : {0}", recieved);
+
+    string response = "Invalid token";
+    if (recieved.ToUpper() == "PING")
+    {
+        response = "+PONG\r\n";
+    }
+
+    bytes = Encoding.UTF8.GetBytes(response);
+
+    sock.Send(bytes);
+
+    Console.WriteLine("Sent : {0}", response);
+}
+
