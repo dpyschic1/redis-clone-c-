2bf161c06cdb2fa710c937104b4b95baabb71d3b
Mubashir <mubashirsoomro60@gmail.com>
2025-08-28 00:04:10 +0500

    port is configurable now

--- src/Server.cs
+++ src/Server.cs
@@ -9,45 +9,8 @@ var redisParser = new RedisProtocolParser();
 var commandExecutor = new CommandExecutor();
 var redisSerializer = new RedisSerializer();
 
-var eventLoop = new EventLoop(6379, redisParser, commandExecutor, redisSerializer);
+var port = args.Length > 0 ? int.Parse(args[0]) : 6379;
+
+
+var eventLoop = new EventLoop(port, redisParser, commandExecutor, redisSerializer);
 eventLoop.Run();
-
-// Uncomment this block to pass the first stage
-// TcpListener server = new TcpListener(IPAddress.Any, 6379);
-// server.Start();
-// List<Socket> sockets = [];
-
-// while (true)
-// {
-//     if (server.Pending())
-//     {
-//         var incomingSocket = server.AcceptSocket();
-//         incomingSocket.Blocking = false;
-//         sockets.Add(incomingSocket);
-//     }
-
-
-//     foreach (var sock in sockets)
-//     {
-//         byte[] buff = new byte[1024];
-//         if (sock.Available > 0)
-//         {
-//             int bytesRead = sock.Receive(buff);
-
-//             if (bytesRead == 0)
-//             { 
-//                 sock.Disconnect(false);
-//                 sockets.Remove(sock);
-//                 continue;   
-//             }
-
-//             var redisParser = new RedisProtocolParser();
-//             var commandExecutor = new CommandExecutor();
-//             var redisSerializer = new RedisSerializer();
-//             var parsedCommands = redisParser.Parse(buff);
-//             var responseCommand = commandExecutor.Execute(parsedCommands);
-//             var responseByte = redisSerializer.Serialize(responseCommand);
-//             sock.Send(responseByte);
-//         }
-//     }
-// }
