d1d9392f9e94a3e85bf5a223616096e47dde417f
Mubashir <mubashirsoomro60@gmail.com>
2025-08-28 00:10:49 +0500

    parsing cmdline args

--- src/Server.cs
+++ src/Server.cs
@@ -9,7 +9,21 @@ var redisParser = new RedisProtocolParser();
 var commandExecutor = new CommandExecutor();
 var redisSerializer = new RedisSerializer();
 
-var port = args.Length > 0 ? int.Parse(args[0]) : 6379;
+var cmdArgs = new Dictionary<string, string>();
+
+for (int i = 0; i < args.Length; i++)
+{
+    if(args[i].StartsWith("--"))
+    {
+        var key =  args[i].Substring(2);
+        var val = (i + 1 < args.Length && !args[i + 1].StartsWith("--")) 
+            ? args[++i] 
+            : "true";
+        cmdArgs.Add(key, val);
+    }
+}
+
+var port = cmdArgs.TryGetValue("port", out var portStr) ? int.Parse(portStr) : 6379;
 
 
 var eventLoop = new EventLoop(port, redisParser, commandExecutor, redisSerializer);
